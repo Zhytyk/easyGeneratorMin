9c403fa6be601959540c5da25fc4f1adc84839e1
zhytyk <zhytyk@gmail.com>
2016-07-06 23:19:02 +0300

    Старт Проект

---
+++ EasyGeneratorMin/App/main.js
@@ -0,0 +1,33 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../scripts/text',
+        'durandal': '../scripts/durandal',
+        'plugins': '../scripts/durandal/plugins',
+        'transitions': '../scripts/durandal/transitions',
+        'knockout': '../scripts/knockout-2.3.0',
+        'jquery': '../scripts/jquery-1.9.1'
+    }
+});
+
+define('jquery', [], function () { return jQuery; });
+define('knockout', [], function () { return ko; });
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator', 'durandal/composition'], function (system, app, viewLocator, composition) {
+
+    //>>excludeStart("build", true);
+    system.debug(true);
+    //>>excludeEnd("build");
+
+    app.start().then(function () {
+        //Replace 'viewmodels' in the moduleId with 'views' to locate the view.
+        //Look for partial views in a 'views' folder in the root.
+        viewLocator.useConvention();
+
+        router.map([
+               { route: '', title: 'MainDurandal', moduleId: 'viewmodels/shell', nav: true },
+        ]).buildNavigationModel();
+
+        //Show the app by setting the root view model for our application with a transition.
+        app.setRoot('viewmodels/shell', 'entrance');
+    });
+});
---
+++ EasyGeneratorMin/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных с этой сборкой.
+[assembly: AssemblyTitle("EasyGeneratorMin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EasyGeneratorMin")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения false в параметре ComVisible делает типы в этой сборке невидимыми 
+// для компонентов COM. Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение true для требуемого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов typelib, если этот проект видим для COM
+[assembly: Guid("f6fb56f3-4576-4cf2-8047-ea382eee0d19")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      основной номер версии;
+//      дополнительный номер версии;
+//      номер сборки;
+//      редакция.
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
