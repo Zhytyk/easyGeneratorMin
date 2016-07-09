b1815e58f65f120e820066da43b3295c55a81f0a
zhytyk <zhytyk@gmail.com>
2016-07-09 18:03:52 +0300

    Remove unnecessary files, webActivatorPackage

--- EasyGeneratorMin/App_Start/BundleConfig.cs
+++ EasyGeneratorMin/App_Start/BundleConfig.cs
@@ -8,8 +8,12 @@ namespace EasyGeneratorMin
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/vendor")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/knockout-{version}.js")
+                .Include("~/Scripts/sammy-{version}.js")
+                );
+
             bundles.Add(
               new StyleBundle("~/Content/css")
               .Include("~/Content/style.css")
