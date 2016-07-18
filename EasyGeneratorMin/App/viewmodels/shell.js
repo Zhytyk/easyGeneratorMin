9ec3482f782c8520b488bb94e3414965e5c6fd73
zhytyk <zhytyk@gmail.com>
2016-07-18 20:03:38 +0300

    rename title in shell/router

--- EasyGeneratorMin/App/viewmodels/shell.js
+++ EasyGeneratorMin/App/viewmodels/shell.js
@@ -4,7 +4,7 @@
         activate: function () {
             router.map([
                { route: '', title: 'Main View', moduleId: 'viewmodels/mainpage', nav: true },
-               { route: 'create', title: 'Add course', moduleId: 'viewmodels/createcoursepage', nav: true },
+               { route: 'create', title: 'Create course', moduleId: 'viewmodels/createcoursepage', nav: true },
                { route: 'update/:id', title: 'Update course', moduleId: 'viewmodels/updatecoursepage', nav: true }
             ]).buildNavigationModel();
 
