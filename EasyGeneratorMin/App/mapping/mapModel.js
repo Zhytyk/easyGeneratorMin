ea40c474eef010353839e41293f50cc18469c414
zhytyk <zhytyk@gmail.com>
2016-07-21 18:43:39 +0300

    fix date in js

--- EasyGeneratorMin/App/mapping/mapModel.js
+++ EasyGeneratorMin/App/mapping/mapModel.js
@@ -1,13 +1,12 @@
 ﻿define(['models/course'], function (Course) {
     function mapCourse(course) {
-        console.log();
         return new Course({
             id: course.Id,
             title: course.Title,
             description: course.Description,
             creater: course.Creater,
-            createdDate: new Date(parseInt(course.CreatedDate.replace(/\/Date\((-?\d+)\)\//, '$1'))),
-            lastUpdatedDate: new Date(parseInt(course.LastUpdatedDate.replace(/\/Date\((-?\d+)\)\//, '$1')))
+            createdDate: new Date(parseInt(course.CreatedDate.replace(/\/Date\((-?\d+)\)\//, '$1'))).toLocaleString(),
+            lastUpdatedDate: new Date(parseInt(course.LastUpdatedDate.replace(/\/Date\((-?\d+)\)\//, '$1'))).toLocaleString()
         });
     };
     return {
